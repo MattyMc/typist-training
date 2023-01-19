6c1f72a11878fe9e01bbd0cd9bc91a9468f2a8d4
Matt McInnis <matt@typistapp.ca>
2023-01-19 11:43:49 -0500

    big update to timed writing section

--- docs/components/problems.js
+++ docs/components/problems.js
@@ -1,5 +1,37 @@
 // multiple choice questions and answers saved in objects. Need to be imported to the md/mdx page where the components appear.
 
+export const timedWritingAttempts = {
+  question:
+    "Suppose a student completes 5 attempts from the same test. The instructor set this test with a minimum accuracy of 98%. Which attempt will you see on your grade sheet? Hint: Only the attempts with a minimum 98% accuracy count on this test. From those attempts, we choose the one with the highest netWPM.",
+  answers: [
+    {
+      text: "1: 33 netWPM at 98.1% accuracy",
+      correct: false,
+      selected: false,
+    },
+    {
+      text: "2: 39 netWPM at 97.1% accuracy",
+      correct: false,
+      selected: false,
+    },
+    {
+      text: "3: 37 netWPM at 98.3% accuracy",
+      correct: false,
+      selected: false,
+    },
+    {
+      text: "4: 37 netWPM at 98.3% accuracy",
+      correct: true,
+      selected: false,
+    },
+    {
+      text: "5: 41 netWPM at 97.4% accuracy",
+      correct: false,
+      selected: false,
+    },
+  ],
+};
+
 export const problem1 = {
   question:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas felis?",
